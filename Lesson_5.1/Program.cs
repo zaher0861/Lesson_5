56bc8838962fb432284c113603f07dea10495f81
Александр Захаров <zaher0861@yandex.ru>
2021-11-28 18:42:19 +0300

    Добавьте файлы проекта.

---
+++ Lesson_5.1/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lesson_5._1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double[] array = new double[7];
+            Console.WriteLine("Введите любое число: ");
+            array[0] = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Введите любое число: ");
+            array[1] = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Введите любое число: ");
+            array[2] = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Введите любое число: ");
+            array[3] = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Введите любое число: ");
+            array[4] = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Введите любое число: ");
+            array[5] = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Введите любое число: ");
+            array[6] = Convert.ToDouble(Console.ReadLine());
+            Console.Write("Ваш массив: ");
+            double Sum = 0;
+            for (int i = 0; i < 7; i++)
+            {
+                Console.Write("{0} ", array[i]);
+                Sum += array[i];
+            }
+            Console.WriteLine();
+            Console.WriteLine("Среднее арифметическое значение чисел в массиве = {0:f3}", Sum/7);
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Lesson_5.5/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lesson_5._5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Введите целое число N, чтобы задать размеры двумерного массива N*N: ");
+            int N = Convert.ToInt32(Console.ReadLine());
+            bool t = true;
+            int[,] array = new int[N, N];
+            for (int i = 0; i < N; i++)
+            {
+                for (int j = 0; j < N; j++)
+                {
+                    if (i == j || t==false)
+                    {
+                        array[i, j] = 1;
+                        t = true;
+                    }
+                    else
+                    {
+                        array[i, j] = 0;
+                        t = false;
+                    }
+                    Console.Write("{0,4}", array[i, j]);
+
+                }
+                Console.WriteLine();
+                if (i == N-1)
+                {
+                    break;
+                }
+                if (array[i,0] == 0 && i<N-1)
+                {
+                    array[i + 1, N-N] = 1;
+                    t = false;
+                }
+                else
+                {
+                    array[i + 1, N-N] = 0;
+                    t = true;
+                }
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
